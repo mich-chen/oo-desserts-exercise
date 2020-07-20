f017300699945de0ffbaafee600fda739e71d9fd
Michelle Chen <mich.chen.94@gmail.com>
2020-07-20 11:46:23 -0700

    added instance methods for instance attributes and add_stock instance method

--- desserts.py
+++ desserts.py
@@ -7,10 +7,24 @@ class Cupcake:
     # dictionary that will store all cupcake instances by name
     cache = {}
 
+    def __init__(self, name, flavor, price, qty=0):
+      """Instance of a cupcake."""
+
+      self.name = name
+      self.flavor = flavor
+      self.price = price
+      self.qty = qty
+      self.cache[self.name] = self
+
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+    def add_stock(self, amount):
+      """Add amount to cupcake stock quantity."""
+
+      self.qty += amount
 
 
 if __name__ == '__main__':
