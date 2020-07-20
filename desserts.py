fd0b22fe4fa678fb32f76f0143134c391d3b6466
Michelle Chen <mich.chen.94@gmail.com>
2020-07-20 12:11:08 -0700

    added static method for scaling recipe

--- desserts.py
+++ desserts.py
@@ -7,14 +7,36 @@ class Cupcake:
     # dictionary that will store all cupcake instances by name
     cache = {}
 
-    def __init__(self, name, flavor, price, qty=0):
-      """Instance of a cupcake."""
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+        """Scale list of ingredients by given amount of cupcakes.
 
-      self.name = name
-      self.flavor = flavor
-      self.price = price
-      self.qty = qty
-      self.cache[self.name] = self
+        Ingredients are given as a list of tuples with (ingredient_name, 
+        ingredient_qty) Return a list of tuples with quantity of each
+        ingredient multiplied by amount.
+        """
+        scaled_recipe = []
+
+        for ingredient in ingredients:
+            ingredient_qty = ingredient[1]
+            scaled_amount = ingredient_qty * amount
+            scaled_ingredient = (ingredient[0], scaled_amount)
+            scaled_recipe.append(scaled_ingredient)
+
+        return scaled_recipe
+
+
+
+
+    def __init__(self, name, flavor, price, qty=0):
+        """Instance of a cupcake."""
+
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = qty
+        self.cache[self.name] = self
+
 
     def __repr__(self):
         """Human-readable printout for debugging."""
@@ -22,21 +44,24 @@ class Cupcake:
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
     def add_stock(self, amount):
-      """Add amount to cupcake stock quantity."""
+        """Add amount to cupcake stock quantity."""
 
-      self.qty += amount
+        self.qty += amount
 
     def sell(self, amount):
-      """Sell the given amount of cupcakes and update stock quantity."""
+        """Sell the given amount of cupcakes and update stock quantity."""
 
-      # if cupcakes are out of stock
-      if self.qty == 0:
-        print('Sorry, these cupcakes are sold out')
+        # if cupcakes are out of stock
+        if self.qty == 0:
+            print('Sorry, these cupcakes are sold out')
 
-      else:
-        self.qty -= amount
-        if self.qty < 0:
-          self.qty = 0
+        else:
+        # subtract amount sold from stock quantity
+            self.qty -= amount
+        # make sure quantity never goes below zero (no negative stock)
+            if self.qty < 0:
+          # if goes below zero (sold more than have) then set to 0
+                self.qty = 0
 
 
 
